3fa24ccc5d62272b273faea16e38be4bf4c73455
Kaizzky <daitran6734@gmail.com>
2025-05-01 12:00:11 -0500

    Update add.test.js

--- test/add.test.js
+++ test/add.test.js
@@ -3,3 +3,7 @@ const add = require('../src/add');
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(add(1, 2)).toBe(4);
+});
